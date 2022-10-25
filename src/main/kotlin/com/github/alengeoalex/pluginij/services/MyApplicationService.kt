5df30191d95307401a45878f79832fa339f69716
GitHub Action <action@github.com>
2022-10-25 14:42:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/alengeoalex/pluginij/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/alengeoalex/pluginij/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.alengeoalex.pluginij.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.alengeoalex.pluginij.MyBundle
 
 class MyApplicationService {
 
